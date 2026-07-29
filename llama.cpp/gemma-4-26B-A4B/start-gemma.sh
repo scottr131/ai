fb68cfac5b2a70b45a7942d03c26cf1318669f9a
Scott Reasoner <sreasoner@outlook.com>
2026-07-29 13:47:08 -0400

    update scripts

--- llama.cpp/gemma-4-26B-A4B/start-gemma.sh
+++ llama.cpp/gemma-4-26B-A4B/start-gemma.sh
@@ -7,4 +7,4 @@ QUANT="Q4_K"
 DEPTH="XL"
 MMPROJ="mmproj-F16"
 
-llama-server --model ${MODEL}-${PARAMS}-${TUNE}-${SUFFIX}-${QUANT}_${DEPTH}.gguf --mmproj ${MMPROJ}.gguf --model-draft mtp-${MODEL}-${PARAMS}-${TUNE}.gguf --temp 1.0 --top-p 0.95 --top-k 64 --spec-type draft-mtp --spec-draft-n-max 2 -t 4
+llama-server --model ${MODEL}-${PARAMS}-${TUNE}-${SUFFIX}-${QUANT}_${DEPTH}.gguf --mmproj ${MMPROJ}.gguf --model-draft mtp-${MODEL}-${PARAMS}-${TUNE}.gguf --temp 1.0 --top-p 0.95 --top-k 64 --spec-type draft-mtp --spec-draft-n-max 2
--- llama.cpp/start-router-numa.sh
+++ llama.cpp/start-router-numa.sh
@@ -2,4 +2,4 @@
 # Use this script in a multi-socket system to prevent the model
 # from crossing NUMA node boundaries.
 
-numactl --cpunodebind=$1 --membind=$1 llama-server --models-preset ./models.ini $@
+numactl --cpunodebind=$1 --membind=$1 llama-server --models-preset ./models.ini ${@:2}
