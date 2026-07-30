622a7a26bf5d3c9ce7c9afb76beb6cbdabfef23b
Scott Reasoner <sreasoner@outlook.com>
2026-07-29 20:22:58 -0400

    update scripts for numactl support

--- llama.cpp/gemma-4-26B-A4B/start-gemma.sh
+++ llama.cpp/gemma-4-26B-A4B/start-gemma.sh
@@ -7,4 +7,23 @@ QUANT="Q4_K"
 DEPTH="XL"
 MMPROJ="mmproj-F16"
 
-llama-server --model ${MODEL}-${PARAMS}-${TUNE}-${SUFFIX}-${QUANT}_${DEPTH}.gguf --mmproj ${MMPROJ}.gguf --model-draft mtp-${MODEL}-${PARAMS}-${TUNE}.gguf --temp 1.0 --top-p 0.95 --top-k 64 --spec-type draft-mtp --spec-draft-n-max 2
+LLAMA_ARGS=(
+	--model ${MODEL}-${PARAMS}-${TUNE}-${SUFFIX}-${QUANT}_${DEPTH}.gguf
+	--mmproj ${MMPROJ}.gguf
+	--model-draft mtp-${MODEL}-${PARAMS}-${TUNE}.gguf
+	--temp 1.0
+	--top-p 0.95
+	--top-k 64
+	--spec-type draft-mtp
+	--spec-draft-n-max 2
+)
+
+if [[ "$1" =~ ^[0-9]$ ]]; then
+    NODE=$1
+    shift
+    echo "Running on NUMA node: $NODE"
+    numactl --cpunodebind="$NODE" --membind="$NODE" llama-server "${LLAMA_ARGS[@]}" "$@"
+else
+    echo "Running without numactl..."
+    llama-server "${LLAMA_ARGS[@]}" "$@"
+fi
--- llama.cpp/start-router.sh
+++ llama.cpp/start-router.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-llama-server --models-preset ./models.ini $@
+if [[ "$1" =~ ^[0-9]$ ]]; then
+    NODE=$1
+    shift
+    echo "Running on NUMA node: $NODE"
+    numactl --cpunodebind="$NODE" --membind="$NODE" llama-server --models-preset ./models.ini $@
+else
+    echo "Running without numactl..."
+    llama-server --models-preset ./models.ini $@
+fi
